2194497e743b777c6c89ac93ded529e1ad272a4b
stanleygoat <stanislaw.koziol@hmbd.pl>
2018-11-13 23:43:38 +0100

    buttons css's and changes

---
+++ js/app.js
@@ -0,0 +1,30 @@
+'use strict';
+
+document.addEventListener('DOMContentLoaded', function(){
+      console.log('DOMContentLoaded');
+
+    // var ledPin = 13;
+    // var wiatrakwejscie = ;
+    // var wiatrakireszta = ;
+    // var lampsup = ;
+    // var lampcorner = ;
+    // var firmata = require('/usr/local/lib/node_modules/firmata');
+    // var board = new firmata.Board('/dev/ttyUSB0', function(err) {
+    //     if (err) {
+    //         console.log(err);
+    //         return;
+    //     }
+    //     console.log('connected');
+    //     console.log('Firmware: ' + board.firmware.name + '-' + board.firmware.version.major + '.' + board.firmware.version.minor);
+     
+    //     var ledOn = true;
+    //     board.pinMode(ledPin, board.MODES.OUTPUT);
+
+    //         board.digitalWrite(ledPin, board.HIGH);
+    //         board.digitalWrite(ledPin, board.LOW);
+     
+    // });
+
+    
+    
+})
